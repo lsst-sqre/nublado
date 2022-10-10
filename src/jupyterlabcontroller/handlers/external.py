e89316de3b7cc016031a41e8c5a538ffe97ebdc6
adam <athornton@lsst.org>
2022-10-10 14:50:11 -0700

    stub out routes

--- src/jupyterlabcontroller/handlers/external.py
+++ src/jupyterlabcontroller/handlers/external.py
@@ -1,12 +1,14 @@
 """Handlers for the app's external root, ``/jupyterlab-controller/``."""
 
 from fastapi import APIRouter, Depends
+from fastapi.responses import RedirectResponse
 from safir.dependencies.logger import logger_dependency
 from safir.metadata import get_metadata
 from structlog.stdlib import BoundLogger
+from typing import List
 
 from ..config import config
-from ..models import Index
+from ..models import Index, UserData, LabSpecification, Event
 
 __all__ = ["get_index", "external_router"]
 
@@ -27,7 +29,7 @@ external_router = APIRouter()
 async def get_index(
     logger: BoundLogger = Depends(logger_dependency),
 ) -> Index:
-    """GET ``/jupyterlab-controller/`` (the app's external root).
+    """GET ``/nublado/`` (the app's external root).
 
     Customize this handler to return whatever the top-level resource of your
     application should return. For example, consider listing key API URLs.
@@ -48,3 +50,75 @@ async def get_index(
         application_name=config.name,
     )
     return Index(metadata=metadata)
+
+@external_router.get(
+    "/spawner/v1/labs",
+    response_model=List[str],
+    summary="List all users with running labs"
+)
+async def get_lab_users() -> List[str]:
+    """requires admin:notebook"""
+    return []
+
+@external_router.get(
+    "/spawner/v1/labs/{username}",
+    response_model=UserData,
+    responses={404: {"description": "Lab not found", "model": ErrorModel}},
+    summary="Status of user",
+)
+async def get_userdata(
+    username: str,
+) -> UserData:
+    """Requires admin:jupyterlab"""
+    return UserData()
+
+@external_router.post(
+    "/spawner/v1/labs/{username}/create",
+    responses={409: {"description": "Lab exists", "model": ErrorModel}},
+    response_class=RedirectResponse,
+    status_code=303,
+    summary="Create user lab",
+)
+async def post_new_lab(
+    username: str,
+    ) -> Redirect:
+    """POST body is a LabSpecification.  Requires exec:notebook and valid
+    user token."""
+    return "http://localhost"
+
+@external_router.get(
+    "/spawner/v1/labs/{username}/events",
+    summary="Get Lab event stream for a user's current operation",
+)
+async def get_user_events(
+    username: str,
+) -> List[Event]:
+    """Requires exec:notebook and valid user token"""
+    return []
+
+@external_router.delete(
+    "/spawner/v1/labs/{username}",
+    responses={404: {"description": "Lab not found", "model": ErrorModel}},
+    status_code=202,
+)
+async def delete_user_lab(
+    username:str,
+) -> None:
+    """Requires admin:notebook"""
+    return
+
+@external_router.get(
+    "/spawner/v1/lab-form/{username}",
+)
+async def get_user_lab_form(
+    username:str,
+    ) -> str:
+    """Requires exec:notebook and valid token."""
+    return ""
+
+@external_router.get(
+    "/spawner/v1/user-status",
+    responses={404: {"description": "Lab not found", "model": ErrorModel}},
+)
+async def get_user_status() -> UserData:
+    return UserData()
