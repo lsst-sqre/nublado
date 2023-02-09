c7185da377cde72cae87b5eaea4cae2e5988f4af
Russ Allbery <rra@lsst.org>
2023-02-09 15:53:41 -0800

    Refactor Docker credential config parsing
    Move the code to create a DockerCredentials from a configuration
    entry into DockerCredentials and simplfy DockerCredentialsMap.

--- src/jupyterlabcontroller/models/domain/docker.py
+++ src/jupyterlabcontroller/models/domain/docker.py
@@ -3,7 +3,7 @@
 import base64
 import json
 from dataclasses import dataclass
-from typing import Dict, Optional
+from typing import Dict, Optional, Self
 
 from structlog.stdlib import BoundLogger
 
@@ -34,13 +34,36 @@ class DockerCredentials:
         auth_data = f"{self.username}:{self.password}".encode()
         return base64.b64encode(auth_data).decode()
 
+    @classmethod
+    def from_config(cls, host: str, config: dict[str, str]) -> Self:
+        """Create from a Docker config entry (such as a pull secret).
+
+        This requires the ``auth`` field be set and ignores the ``username``
+        and ``password`` field.
+
+        Parameters
+        ----------
+        host
+            The hostname of the server for which these credentials apply.
+        config
+            The entry for that hostname in the configuration.
+
+        Returns
+        -------
+        DockerCredentials
+            The resulting credentials.
+        """
+        basic_auth = base64.b64decode(config["auth"].encode()).decode()
+        username, password = basic_auth.split(":", 1)
+        return cls(registry_host=host, username=username, password=password)
+
 
 class DockerCredentialsMap:
     def __init__(
         self, logger: BoundLogger, filename: str = DOCKER_SECRETS_PATH
     ) -> None:
         self.logger = logger
-        self._credentials: Dict[str, DockerCredentials] = dict()
+        self._creds: Dict[str, DockerCredentials] = dict()
         if filename == "":
             return
         try:
@@ -49,21 +72,16 @@ class DockerCredentialsMap:
             self.logger.warning(f"No credentials file at {filename}")
 
     def get(self, host: str) -> Optional[DockerCredentials]:
-        for h in self._credentials:
+        for h in self._creds:
             if h == host or host.endswith(f".{h}"):
-                return self._credentials[h]
+                return self._creds[h]
         return None
 
     def load_file(self, filename: str) -> None:
         with open(filename) as f:
             credstore = json.loads(f.read())
-        self._credentials = dict()
+        self._creds = {}
         self.logger.debug("Removed existing Docker credentials")
-        for host in credstore["auths"]:
-            b64auth = credstore["auths"][host]["auth"]
-            basic_auth = base64.b64decode(b64auth).decode()
-            username, password = basic_auth.split(":", 1)
-            self._credentials[host] = DockerCredentials(
-                registry_host=host, username=username, password=password
-            )
+        for host, config in credstore["auths"].items():
+            self._creds[host] = DockerCredentials.from_config(host, config)
             self.logger.debug(f"Added authentication for '{host}'")
