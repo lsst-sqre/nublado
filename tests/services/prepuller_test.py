86325ff83a6ada397564a92ac51d36aa0c303735
Russ Allbery <rra@lsst.org>
2023-02-21 09:55:11 -0800

    Undo conversion of DisplayImages to BaseModel
    Stick with the rule that internal models are dataclasses and API
    models are Pydantic models at the cost of a slightly more
    complicated test. This should be temporary anyway until we start
    testing against the API.

--- tests/services/prepuller_test.py
+++ tests/services/prepuller_test.py
@@ -12,7 +12,10 @@ async def test_get_menu_images(factory: Factory, std_result_dir: Path) -> None:
         expected = json.load(f)
     prepuller_arbitrator = factory.create_prepuller_arbitrator()
     r = prepuller_arbitrator.get_menu_images()
-    assert r.dict() == expected
+    assert {
+        "menu": {k: v.dict() for k, v in r.menu.items()},
+        "all": {k: v.dict() for k, v in r.all.items()},
+    } == expected
 
 
 @pytest.mark.asyncio
