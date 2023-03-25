13454b31c12b6cd00f3e3699320f92daec317d55
Russ Allbery <rra@lsst.org>
2023-03-25 11:27:07 -0700

    Remove duplicate user status route
    The labs handler defined another copy of the user status route at
    a strange URL. Remove that in favor of the one defined in the
    user status handler.

--- src/jupyterlabcontroller/handlers/labs.py
+++ src/jupyterlabcontroller/handlers/labs.py
@@ -138,23 +138,3 @@ async def get_user_events(
         raise HTTPException(status_code=404, detail="Not found")
 
     return context.event_manager.publish(username)
-
-
-@router.get(
-    "/spawner/v1/user-status",
-    responses={
-        404: {"description": "Lab not found", "model": ErrorModel},
-        403: {"description": "Forbidden", "model": ErrorModel},
-    },
-    summary="Get status for user",
-    response_model=UserData,
-)
-async def get_user_status(
-    x_auth_request_user: str = Header(...),
-    context: RequestContext = Depends(context_dependency),
-) -> UserData:
-    """Get the pod status for the authenticating user."""
-    userdata = context.user_map.get(x_auth_request_user)
-    if userdata is None:
-        raise HTTPException(status_code=404, detail="Not found")
-    return userdata
