bd72a10c63442593924fccc2a4bbfd32497aba67
Russ Allbery <rra@lsst.org>
2023-03-25 12:17:26 -0700

    Test form generation through a handler
    We may as well test form generation by going through the front door
    of the application instead of bypassing it and calling directly into
    the service. This also allows deleting the direct test of the image
    service method for generating menu images, since those results are
    incorporated into the user form.

--- tests/services/prepuller_test.py
+++ tests/services/prepuller_test.py
@@ -1,21 +1,9 @@
 import json
-from dataclasses import asdict
 from pathlib import Path
 
 import pytest
 
 from jupyterlabcontroller.factory import Factory
-
-
-@pytest.mark.asyncio
-async def test_get_menu_images(factory: Factory, std_result_dir: Path) -> None:
-    with (std_result_dir / "menu-images.json").open("r") as f:
-        expected = json.load(f)
-    images = factory.image_service.menu_images()
-    assert {
-        "menu": [asdict(i) for i in images.menu],
-        "dropdown": [asdict(i) for i in images.dropdown],
-    } == expected
 
 
 @pytest.mark.asyncio
