ad6ce3f8490d2066b63c55eae3d95f4272266c74
adam <athornton@lsst.org>
2023-07-26 10:39:15 -0700

    Add test for Pod Spec workingDir

--- tests/services/homedir_test.py
+++ tests/services/homedir_test.py
@@ -5,6 +5,9 @@ from __future__ import annotations
 import pytest
 
 from jupyterlabcontroller.factory import Factory
+from jupyterlabcontroller.models.domain.rspimage import RSPImage
+from jupyterlabcontroller.models.domain.rsptag import RSPImageTag
+from jupyterlabcontroller.models.v1.lab import LabResources, ResourceQuantity
 
 from ..settings import TestObjectFactory
 from ..support.config import configure
@@ -22,3 +25,40 @@ async def test_homedir_schema_nss(obj_factory: TestObjectFactory) -> None:
         lm = factory.create_lab_manager()
         nss = lm.build_nss(user)
         assert nss == expected_nss
+
+
+@pytest.mark.asyncio
+async def test_homedir_schema_pod(obj_factory: TestObjectFactory) -> None:
+    """Test building a pod spec with the alternate homedir layout and make
+    sure `working_dir` is correctly set."""
+    _, user = obj_factory.get_user()
+    config = configure("homedir-schema")
+
+    async with Factory.standalone(config) as factory:
+        # We will construct image and resources fairly directly, rather than
+        # going through the layers to get them 'legitimately'
+        contents = obj_factory.test_objects["user_options"][0]["image_list"]
+        tag = RSPImageTag.from_str(contents)
+        registry, repo, rest = contents.split("/")
+        ttag, digest = rest.split("@")
+        image = RSPImage(
+            registry=registry,
+            repository=repo,
+            digest=digest,
+            size=None,
+            aliases=set(),
+            version=None,
+            cycle=None,
+            tag=ttag,
+            image_type=tag.image_type,
+            display_name=ttag,
+        )
+        resources = LabResources(
+            limits=ResourceQuantity(cpu=1.0, memory=4294967296),
+            requests=ResourceQuantity(cpu=0.25, memory=1073741824),
+        )
+        lm = factory.create_lab_manager()
+        podspec = lm.build_pod_spec(user, resources, image)
+        # The Pod spec isn't JSON-serializable, so we will just check the
+        # field we care about rather than comparing the objects.
+        assert podspec.containers[0].working_dir == "/home/r/rachel"
