97cc9ceecfaac4b039b0d328038766deef1433a3
Adam Thornton <athornton@lsst.org>
2024-10-01 12:51:38 -0700

    Add dircolors and compatibility shim

--- jupyterlab-base/profile.d/local05-term.sh
+++ jupyterlab-base/profile.d/local05-term.sh
@@ -4,3 +4,8 @@ if [ "${TERM}" == "xterm" ]; then
 fi
 export TERM
 
+# Possibly controversial.  Feel free to override, change the color definitions,
+#  or disable completely in your own .bash_profile.
+eval $(dircolors -b /etc/dircolors.ansi-universal)
+alias ls="ls --color=auto"
+
