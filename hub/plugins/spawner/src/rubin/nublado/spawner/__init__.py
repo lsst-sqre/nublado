cb5a0a3487144747d7d24684768cf7403d71e7f2
Russ Allbery <rra@lsst.org>
2025-10-08 12:01:02 -0700

    Export all spawner plugin exceptions
    List all of the exceptions that can be raised by the Nublado JupyterHub
    spawner plugin in the module's `__init__.py`.

--- hub/plugins/spawner/src/rubin/nublado/spawner/__init__.py
+++ hub/plugins/spawner/src/rubin/nublado/spawner/__init__.py
@@ -1,10 +1,19 @@
 """JupyterHub spawner that uses the Nublado controller to manage labs."""
 
-from ._exceptions import ControllerWebError, InvalidAuthStateError
+from ._exceptions import (
+    ControllerWebError,
+    DiscoveryError,
+    InvalidAuthStateError,
+    MissingFieldError,
+    SpawnFailedError,
+)
 from ._internals import NubladoSpawner
 
 __all__ = [
     "ControllerWebError",
+    "DiscoveryError",
     "InvalidAuthStateError",
+    "MissingFieldError",
     "NubladoSpawner",
+    "SpawnFailedError",
 ]
