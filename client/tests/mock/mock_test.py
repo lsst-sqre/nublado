f3441a41541497c2742d064ca9e66f2b68ac08c5
Russ Allbery <rra@lsst.org>
2025-10-30 16:17:37 -0700

    Delete JupyterLabSession.run_notebook
    This method on the session ran a notebook one cell at a time. This
    was not used by any of the existing `NubladoClient` users. The
    `NubladoClient.run_notebook` method is preferred for whole notebook
    execution.

--- client/tests/mock/mock_test.py
+++ client/tests/mock/mock_test.py
@@ -64,60 +64,6 @@ async def test_register_python(
 
 
 @pytest.mark.asyncio
-async def test_register_python_with_notebook(
-    configured_client: NubladoClient, jupyter: MockJupyter
-) -> None:
-    """Register 'python' code with the mock and check its output."""
-    obj = json.loads(INPUT_NB.read_text())
-    sources = [
-        "\n".join(x["source"]).strip().rstrip("\n")
-        for x in obj["cells"]
-        if x["cell_type"] == "code"
-        and "".join(x["source"]).strip().rstrip("\n")
-    ]
-    sources[-1] = sources[-1].rstrip("\n")
-    assert len(sources) == 1
-
-    # Register our code with the mock.
-    jupyter.register_python_result(sources[0], "42")
-
-    # Do the whole lab flow
-    await configured_client.auth_to_hub()
-    assert await configured_client.is_lab_stopped()
-    # Simulate spawn
-    await configured_client.spawn_lab(
-        NubladoImageByClass(
-            image_class=NubladoImageClass.RECOMMENDED,
-            size=NubladoImageSize.Medium,
-            description="Recommended (Weekly 2077_44)",
-        )
-    )
-    # Watch the progress meter
-    progress = configured_client.watch_spawn_progress()
-    progress_pct = -1
-    async with aclosing(progress):
-        async with asyncio.timeout(30):
-            async for message in progress:
-                if message.ready:
-                    break
-                assert message.progress > progress_pct
-                progress_pct = message.progress
-    await configured_client.auth_to_lab()
-
-    # Now test our mock
-    async with configured_client.open_lab_session() as lab_session:
-        forty_two = (await lab_session.run_notebook(Path("faux-input.ipynb")))[
-            0
-        ].strip()
-        assert forty_two == "42"
-
-    # Stop the lab
-    await configured_client.stop_lab()
-    # Is the lab running?  Should not be.
-    assert await configured_client.is_lab_stopped()
-
-
-@pytest.mark.asyncio
 async def test_register_extension(
     configured_client: NubladoClient, jupyter: MockJupyter
 ) -> None:
