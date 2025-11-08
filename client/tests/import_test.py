671271575fafe9730ca7d595ad947d0d11765edd
Russ Allbery <rra@lsst.org>
2025-11-07 18:14:22 -0800

    Restructure the remaining client tests
    Merge all the exception tests into a single file and clean up the
    code a little bit. Remove the unnecessary import test.

--- client/tests/import_test.py
+++
@@ -1,9 +0,0 @@
-"""Basic import functionality."""
-
-import rubin.nublado.client
-
-
-def test_import() -> None:
-    """The test is really the above import."""
-    nc = rubin.nublado.client.NubladoClient
-    assert nc is not None
