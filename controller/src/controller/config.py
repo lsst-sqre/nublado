5bee4a0203fd5705cce71e65a5a5f2e68d762401
adam <athornton@lsst.org>
2025-11-25 15:49:13 -0700

    Allow NSS overmounts in Lab init containers

--- controller/src/controller/config.py
+++ controller/src/controller/config.py
@@ -59,6 +59,7 @@ __all__ = [
     "HostPathVolumeSource",
     "LabConfig",
     "LabInitContainer",
+    "LabInitContainerVolumeMountConfig",
     "LabNSSFiles",
     "LabSecret",
     "LabSizeDefinition",
@@ -314,6 +315,27 @@ class VolumeMountConfig(BaseModel):
     volume_name: Annotated[
         str,
         Field(title="Volume name", description="Name of the volume to mount"),
+    ]
+
+
+class LabInitContainerVolumeMountConfig(VolumeMountConfig):
+    """The mount of a volume inside an init container.
+
+    This is exactly the same as VolumeMountConfig but we do not prohibit
+    overmounting, since we may want to mount the NSS files into /etc.
+    """
+
+    container_path: Annotated[
+        str,
+        Field(
+            title="Path inside container",
+            description=(
+                "Absolute path at which to mount the volume in the lab"
+                " container"
+            ),
+            examples=["/home"],
+            pattern="^/.*",
+        ),
     ]
 
 
@@ -744,7 +766,7 @@ class LabInitContainer(BaseModel):
     ] = False
 
     volume_mounts: Annotated[
-        list[VolumeMountConfig],
+        list[LabInitContainerVolumeMountConfig],
         Field(
             title="Volume mounts",
             description="Volumes mounted inside this init container",
