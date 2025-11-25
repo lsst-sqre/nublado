d062b931bfc725c7df1ce12021e9b09189906cfb
adam <athornton@lsst.org>
2025-11-25 12:23:33 -0700

    Build rsp-initcontainer

---
+++ rsp-initcontainer/scripts/install-lsst-rsp.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -x
+set -euo pipefail
+# Install lsst-rsp, which contains the entrypoints we need for both the
+# landing page provisioner and the RSP init container.
+#
+# Don't bother with a venv: things in lsst-rsp are the only reason you'd
+# run this container.
+
+uv pip install --system 'git+https://github.com/lsst-sqre/lsst-rsp@tickets/DM-53305'
+
+# Increase this to build a new version when we have a new lsst-rsp.  Reset
+# if anything else in this file changes.
+# Serial: 0
