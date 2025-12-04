babb6b15ee738a4a822266119e72c14a801297e0
adam <athornton@lsst.org>
2025-12-03 17:24:09 -0700

    Add new CLI

---
+++ src/nublado/constants.py
@@ -0,0 +1,6 @@
+"""Constants for Nublado.  Overrideable for testing."""
+
+ENV_PREFIX = "NUBLADO"
+ALERT_HOOK_ENV_VAR = f"{ENV_PREFIX}_ALERT_HOOK"
+ROOT_LOGGER = "nublado"
+CLONER_SCRIPT = "/usr/local/bin/repo-cloner.sh"
