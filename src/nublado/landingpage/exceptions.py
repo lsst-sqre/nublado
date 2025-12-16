5ab1edf6fbedd075a105378649bc4f2eee9448b9
Adam Thornton <athornton@lsst.org>
2025-12-16 10:32:56 -0700

    Move landingpage to nublado

---
+++ src/nublado/landingpage/exceptions.py
@@ -0,0 +1,15 @@
+"""Exceptions for the CST landing page provisioner."""
+
+__all__ = ["PrecheckError", "DestinationError", "DestinationIsDirectoryError"]
+
+
+class PrecheckError(Exception):
+    """Initial input-files-and-home-directory sanity check failed."""
+
+
+class DestinationError(Exception):
+    """The destination target exists and cannot be replaced safely."""
+
+
+class DestinationIsDirectoryError(DestinationError):
+    """The destination target exists and is a directory."""
