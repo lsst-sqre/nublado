5eec9810f437ed2869dcf9315d747f27d6948a8f
adam <athornton@lsst.org>
2026-02-25 14:23:09 -0700

    Force cd to homedir in login shell

--- jupyterlab-base/static/etc/profile.d/local01-cd.sh
+++ jupyterlab-base/static/etc/profile.d/local01-cd.sh
@@ -1,7 +1,13 @@
+<<<<<<< HEAD:jupyterlab-base/static/etc/profile.d/local01-cd.sh
 #!/usr/bin/env sh
 
 # If we do not start JupyterLab from ${HOME}, this will ensure that we
 # nevertheless start new shells in ${HOME}
 if [ -d ${HOME} ]; then
     cd ${HOME}
+=======
+#!/bin/sh
+if [ -n "${HOME}" ];
+   cd "${HOME}"
+>>>>>>> 0fb87b3a (Force cd to homedir in login shell):jupyterlab-base/profile.d/local01-cd.sh
 fi
