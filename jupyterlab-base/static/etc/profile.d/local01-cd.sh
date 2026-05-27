4d3d6e92acf32e602067bbcc8b3a859b1c5c88db
adam <athornton@lsst.org>
2026-05-27 12:03:11 -0700

    Tidy up assets

--- jupyterlab-base/static/etc/profile.d/local01-cd.sh
+++ jupyterlab-base/static/etc/profile.d/local01-cd.sh
@@ -1,13 +1,6 @@
-<<<<<<< HEAD:jupyterlab-base/static/etc/profile.d/local01-cd.sh
 #!/usr/bin/env sh
 
 # If we do not start JupyterLab from ${HOME}, this will ensure that we
 # nevertheless start new shells in ${HOME}
 if [ -d ${HOME} ]; then
     cd ${HOME}
-=======
-#!/bin/sh
-if [ -n "${HOME}" ];
-   cd "${HOME}"
->>>>>>> 303e851b (Force cd to homedir in login shell):jupyterlab-base/profile.d/local01-cd.sh
-fi
